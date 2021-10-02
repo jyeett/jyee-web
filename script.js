a64339b71f3e41da8f7df628029836aced1994b2
Jordan Yee <jordanyee799@gmail.com>
2021-10-02 12:12:05 -0700

    add name section

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const h2 = document.createElement("h2");
-h2.textContent = "this content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+// const h2 = document.createElement("h2");
+// h2.textContent = "this content added by JavaScript";
+// document.querySelector("body").appendChild(h2);
+
+function sayHello() {
+
+    console.log(`hello`);
+  
+}
